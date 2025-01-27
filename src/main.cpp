8f1d9f59f2e9e178fb4885954d2f17b8ebc6be5b
Dima Pasumansky <pasumansky@jetstyle.ru>
2025-01-27 21:22:13 +0500

    omg hellow window done

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,69 @@
-// #include <GL/glew.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
 
-// old compile command - g++ main.cpp -o opengl_app -lglfw -lGL -lGLEW
-// new compile command - g++ src/main.cpp src/glad.c -o opengl_app -Iinclude -lglfw -lGL
+// usage: g++ src/main.cpp src/glad.c -o opengl_app -Iinclude -lglfw -lGL
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-  glViewport(0, 0, width, height);
-}
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 int main() {
-  // Initialize GLFW
-  if (!glfwInit()) {
-    std::cerr << "Failed to initialize GLFW" << std::endl;
-    return -1;
-  }
+  // glfw: initialize and configure
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  // Create a GLFW window
-  GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", nullptr, nullptr);
-  if (!window) {
-    std::cerr << "Failed to create GLFW window" << std::endl;
+  GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL)
+  {
+    std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
     return -1;
   }
-
   glfwMakeContextCurrent(window);
-  
-  // Load OpenGL functions using GLAD
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		std::cerr << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
-	
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-  // Rendering loop
-  while (!glfwWindowShouldClose(window)) {
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glfwSwapBuffers(window);
-    glfwPollEvents();
+  
+  // glad: load all OpenGL function pointers
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
   }
+  
+  // render loop
+  while (!glfwWindowShouldClose(window))
+  {
+    processInput(window);
+    
+    // omg - rendering greenish color
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);   // state-setting func
+    glClear(GL_COLOR_BUFFER_BIT);           // state-using func
 
-  glfwDestroyWindow(window);
+    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+    glfwSwapBuffers(window);
+    glfwPollEvents();    
+  }
+  
+  // glfw: terminate, clearing all previously allocated GLFW resources.
   glfwTerminate();
   return 0;
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  // make sure the viewport matches the new window dimensions; note that width and 
+  // height will be significantly larger than specified on retina displays.
+  glViewport(0, 0, width, height);
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame will react accordingly
+void processInput(GLFWwindow *window)
+{
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
 }
