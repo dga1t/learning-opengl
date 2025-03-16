fa09ad35f9371f43dc0b44a5bc5cecc65d0e02bd
Dima Pasumansky <dpsmnsk@gmail.com>
2025-03-16 22:08:07 +0500

    ch2 WIP

--- src/chapter01/main.cpp
+++ src/chapter01/main.cpp
@@ -9,10 +9,6 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
-// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-
 const char *vertexShaderSource = "#version 330 core\n"
   "layout (location = 0) in vec3 aPos;\n"
   "void main()\n"
