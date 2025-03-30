8cc4cfd8e79f8d4ef02147081555c019d877113a
Dima Pasumansky <dpsmnsk@gmail.com>
2025-03-30 23:00:05 +0500

    ch2 wip

--- src/common/shader.cpp
+++ src/common/shader.cpp
@@ -30,7 +30,25 @@ Shader::Shader(const char *vertexSource, const char *fragmentSource) {
   glDeleteShader(fragment);
 }
 
-void Shader::use() { glUseProgram(ID); }
+void Shader::use()
+{
+  glUseProgram(ID);
+}
+
+void Shader::setBool(const std::string &name, bool value) const
+{         
+  glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value); 
+}
+
+void Shader::setInt(const std::string &name, int value) const
+{ 
+  glUniform1i(glGetUniformLocation(ID, name.c_str()), value); 
+}
+
+void Shader::setFloat(const std::string &name, float value) const
+{ 
+  glUniform1f(glGetUniformLocation(ID, name.c_str()), value); 
+} 
 
 void Shader::checkCompileErrors(unsigned int shader, std::string type) {
   int success;
--- src/common/shader.h
+++ src/common/shader.h
@@ -2,16 +2,20 @@
 #define SHADER_H
 
 #include <glad/glad.h>
+
 #include <string>
 
-class Shader {
+class Shader
+{
 public:
+  // the program ID
   unsigned int ID;
 
   Shader(const char *vertexPath, const char *fragmentPath);
+  // use/activate the shader
   void use();
 
-  // Utility functions
+  // utility uniform functions
   void setBool(const std::string &name, bool value) const;
   void setInt(const std::string &name, int value) const;
   void setFloat(const std::string &name, float value) const;
