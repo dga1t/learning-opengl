bfd8fd02dcb18780e223e065e9f2ed4c3dd2f081
Dima Pasumansky <pasumansky@jetstyle.ru>
2025-04-20 22:13:03 +0500

    ch2 move shader source to separate files

--- src/chapter02/main.cpp
+++ src/chapter02/main.cpp
@@ -7,46 +7,7 @@
 #include "../common/utils.h"
 #include "../common/shader.h"
 
-const char *vertexShaderSource ="#version 330 core\n"
-  "layout (location = 0) in vec3 aPos;\n"
-  "void main()\n"
-  "{\n"
-  "   gl_Position = vec4(aPos, 1.0);\n"
-  "}\0";
-
-const char *fragmentShaderSource = "#version 330 core\n"
-  "out vec4 FragColor;\n"
-  "uniform vec4 ourColor;\n"
-  "void main()\n"
-  "{\n"
-  "   FragColor = ourColor;\n"
-  "}\n\0";
-  
-const char *vertexShaderMulticolorSource ="#version 330 core\n"
-  "layout (location = 0) in vec3 aPos;\n"     // the position variable has attribute position 0
-  "layout (location = 1) in vec3 aColor;\n"   // the color variable has attribute position 1
-  "out vec3 ourColor;\n"                      // output a color to the fragment shader
-  "void main()\n"
-  "{\n"
-  "   gl_Position = vec4(aPos, 1.0);\n"
-  "   ourColor = aColor;\n"                   // set ourColor to the input color we got from the vertex data
-  "}\0";
-  
-const char *fragmentShaderMulticolorSource = "#version 330 core\n"
-  "out vec4 FragColor;\n"
-  "in vec3 ourColor;\n"
-  "void main()\n"
-  "{\n"
-  "   FragColor = vec4(ourColor, 1.0);\n"
-  "}\n\0";
-
-int main() {
-  
-  // TODO - fails with segfault err - figure out porque
-  // int nrAttributes;
-  // glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
-  // std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
-  
+int main() {  
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -68,54 +29,7 @@ int main() {
     return -1;
   }
   
-  // TODO - user creating shader code from common/shader.h
-  Shader ourShader(vertexShaderSource, fragmentShaderSource);
-  
-  // vertex shader
-  // unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-  // glShaderSource(vertexShader, 1, &vertexShaderMulticolorSource, NULL);
-  // glCompileShader(vertexShader);
-  // // check for shader compile errors
-  // int  success;
-  // char infoLog[512];
-  // glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-  // if (!success)
-  // {
-  //   glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-  //   std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-  // }
-  
-  // fragment shader
-  // unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-  // glShaderSource(fragmentShader, 1, &fragmentShaderMulticolorSource, NULL);
-  // glCompileShader(fragmentShader);
-  // // check for shader compile errors
-  // glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-  // if (!success)
-  // {
-  //   glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-  //   std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-  // }
-  
-  // link shaders
-  // unsigned int shaderProgram = glCreateProgram();
-  // glAttachShader(shaderProgram, vertexShader);
-  // glAttachShader(shaderProgram, fragmentShader);
-  // glLinkProgram(shaderProgram);
-  // // check for linking errors
-  // glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-  // if (!success) {
-  //   glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-  //   std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-  // }
-  // glDeleteShader(vertexShader);
-  // glDeleteShader(fragmentShader);
-  
-  float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-    0.5f, -0.5f, 0.0f,
-    0.0f,  0.5f, 0.0f
-  };
+  Shader ourShader("src/common/shaders/triangle.vs", "src/common/shaders/triangle.fs");
   
   float verticesMulticolor[] = {
     // positions         // colors
@@ -153,8 +67,7 @@ int main() {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // be sure to activate the shader
-    // glUseProgram(shaderProgram);
+    // render the triangle
     ourShader.use();
     glBindVertexArray(VAO);
     glDrawArrays(GL_TRIANGLES, 0, 3);
--- src/common/shader.cpp
+++ src/common/shader.cpp
@@ -3,28 +3,56 @@
 #include <iostream>
 #include <sstream>
 
-Shader::Shader(const char *vertexSource, const char *fragmentSource) {
+Shader::Shader(const char *vertexPath, const char *fragmentPath) {
+  // 1. retrieve the vertex/fragment source code from filePath
+  std::string vertexCode;
+  std::string fragmentCode;
+  std::ifstream vShaderFile;
+  std::ifstream fShaderFile;
+  // ensure ifstream objects can throw exceptions:
+  vShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+  fShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+  try 
+  {
+    // open files
+    vShaderFile.open(vertexPath);
+    fShaderFile.open(fragmentPath);
+    std::stringstream vShaderStream, fShaderStream;
+    // read file's buffer contents into streams
+    vShaderStream << vShaderFile.rdbuf();
+    fShaderStream << fShaderFile.rdbuf();
+    // close file handlers
+    vShaderFile.close();
+    fShaderFile.close();
+    // convert stream into string
+    vertexCode = vShaderStream.str();
+    fragmentCode = fShaderStream.str();
+  }
+  catch (std::ifstream::failure& e)
+  {
+    std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ: " << e.what() << std::endl;
+  }
+  const char* vShaderCode = vertexCode.c_str();
+  const char * fShaderCode = fragmentCode.c_str();
+  
+  // 2. compile shaders
   unsigned int vertex, fragment;
-
   // Vertex Shader
   vertex = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertex, 1, &vertexSource, NULL);
+  glShaderSource(vertex, 1, &vShaderCode, NULL);
   glCompileShader(vertex);
   checkCompileErrors(vertex, "VERTEX");
-
   // Fragment Shader
   fragment = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragment, 1, &fragmentSource, NULL);
+  glShaderSource(fragment, 1, &fShaderCode, NULL);
   glCompileShader(fragment);
   checkCompileErrors(fragment, "FRAGMENT");
-
   // Shader Program
   ID = glCreateProgram();
   glAttachShader(ID, vertex);
   glAttachShader(ID, fragment);
   glLinkProgram(ID);
   checkCompileErrors(ID, "PROGRAM");
-
   // Delete shaders
   glDeleteShader(vertex);
   glDeleteShader(fragment);
